5fdfde7183df8a494e72a2a3b5acda344839eca7
GuilhermeOlegario <93616664+GuilhermeOlegario@users.noreply.github.com>
2021-11-02 16:51:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var boll;
+function setup() {
+  createCanvas(400,400);
+  boll=createSprite(200,200,30,30);
+  boll.shapeColor="purple";
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
